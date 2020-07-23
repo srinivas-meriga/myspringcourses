8f2a197c728cbb66949dd6e8ef16dd4c4bdbd877
meriga <sreenu.meriga@gmail.com>
2020-07-23 22:26:59 +0530

    added service for user update

--- ryjios-services/src/main/java/com/ryjios/ms/user/UserJPAResource.java
+++ ryjios-services/src/main/java/com/ryjios/ms/user/UserJPAResource.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -59,6 +60,22 @@ public class UserJPAResource {
                 .toUri();
         return ResponseEntity.created(location).build();
     }
+    
+    @PutMapping(path = "/rjam/users/{id}")
+    public ResponseEntity<Object> updateStudent(@RequestBody User user, @PathVariable int id) {
+
+        Optional<User> userOptional = userRepository.findById(id);
+
+        if (!userOptional.isPresent())
+            return ResponseEntity.notFound().build();
+
+        user.setId(id);
+        
+        userRepository.save(user);
+
+        return ResponseEntity.noContent().build();
+    }
+    
     
     @GetMapping(path = "/rjam/users/{emailId}/loginValidate/{password}")
     public User loginValidate(@PathVariable String emailId, @PathVariable String password) {
